d2cb081eb1b324e784f05d7eebfc5ccdd3d669f3
Kenta Goto <130458173+koto-thing@users.noreply.github.com>
2026-09-12 22:09:30 +0900

    feat: Add FMODをマージ
    * Add FMOD Project Files
    
    * Add FMOD Settings
    
    * 海と森の環境音を追加
    AIにメロだけ食わせた仮のBGMを追加
    
    * Unityの方に実装
    
    * Change SE Params

---
+++ Assets/src/FMODServices/FMODVCAService.cs
@@ -0,0 +1,79 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+
+namespace FMODServices
+{
+    /// <summary>
+    /// FMODのVCA（ボリュームコントロール）を管理するサービスクラス
+    /// </summary>
+    public class FMODVCAService
+    {
+        private VCA masterVCA;
+        private VCA bgmVCA;
+        private VCA seVCA;
+        private VCA environmentVCA;
+        
+        /// <summary>
+        /// VCAをRuntimeManagerから取得してリンクする
+        /// </summary>
+        public void LinkVCAs()
+        {
+            masterVCA = RuntimeManager.GetVCA("vca:/Master");
+            bgmVCA = RuntimeManager.GetVCA("vca:/BGM");
+            seVCA = RuntimeManager.GetVCA("vca:/SE");
+            environmentVCA = RuntimeManager.GetVCA("vca:/Environment");
+        }
+
+        public void InitVCAs(float masterVolume = 1.0f, float bgmVolume = 1.0f, float seVolume = 1.0f, float environmentVolume = 1.0f)
+        {
+            masterVCA.setVolume(masterVolume);
+            bgmVCA.setVolume(bgmVolume);
+            seVCA.setVolume(seVolume);
+            environmentVCA.setVolume(environmentVolume);
+        }
+        
+        public void SetMasterVolume(float volume)
+        {
+            masterVCA.setVolume(volume);
+        }
+
+        public void SetBgmVolume(float volume)
+        {
+            bgmVCA.setVolume(volume);
+        }
+
+        public void SetSeVolume(float volume)
+        {
+            seVCA.setVolume(volume);
+        }
+
+        public void SetEnvironmentVolume(float volume)
+        {
+            environmentVCA.setVolume(volume);
+        }
+        
+        public float GetMasterVolume()
+        {
+            masterVCA.getVolume(out float volume);
+            return volume;
+        }
+        
+        public float GetBgmVolume()
+        {
+            bgmVCA.getVolume(out float volume);
+            return volume;
+        }
+        
+        public float GetSeVolume()
+        {
+            seVCA.getVolume(out float volume);
+            return volume;
+        }
+
+        public float GetEnvironmentVolume()
+        {
+            environmentVCA.getVolume(out float volume);
+            return volume;
+        }
+    }
+}
---
+++ Assets/src/SaveSettings/GameData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SaveSettings
+{
+    [Serializable]
+    public class GameData
+    {
+        public AudioSettingsData audioSettings = new();
+    }
+
+    [Serializable]
+    public class AudioSettingsData
+    {
+        public float masterVolume;
+        public float bgmVolume;
+        public float seVolume;
+        public float environmentVolume;
+    }
+}
