969d74e9012ff28dcb405132dd92ce55e63a88c8
koto-thing <gotoukenta62@gmail.com>
2026-09-20 06:49:56 +0900

    Add Navigation Arrow

--- Assets/_Project/Scripts/LifetimeScopes/NavigationArrowLifetimeScope.cs
+++ Assets/_Project/Scripts/LifetimeScopes/NavigationArrowLifetimeScope.cs
@@ -1,5 +1,6 @@
 ﻿using NavigationArrowServices;
 using Presentation;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 using View;
@@ -8,10 +9,16 @@ namespace LifetimeScopes
 {
     public class NavigationArrowLifetimeScope : LifetimeScope
     {
+        [SerializeField] private Transform _playerTransform;
+        [SerializeField] private Vector3 _initialTargetPosition = new Vector3(5f, 0f, 5f);
+
+        /// <summary>案内に必要な初期値をServiceへ渡し、表示との接続を登録する</summary>
+        /// <param name="builder">シーンの依存関係の登録先</param>
+        /// <example>シーン読み込み時にVContainerが呼ぶ</example>
         protected override void Configure(IContainerBuilder builder)
         {
             // NavigationArrow関連のサービス
-            builder.Register<NavigationArrowService>(Lifetime.Singleton);
+            builder.Register(_ => new NavigationArrowService(_playerTransform, _initialTargetPosition), Lifetime.Singleton);
 
             // NavigationArrow関連のPresenter
             builder.RegisterEntryPoint<NavigationArrowPresenter>();
--- Assets/_Project/Scripts/NavigationArrowService/NavigationArrowService.cs
+++ Assets/_Project/Scripts/NavigationArrowService/NavigationArrowService.cs
@@ -1,25 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace NavigationArrowServices
 {
     public class NavigationArrowService
     {
-        public void UpdateArrowTransform()
-        {
+        private readonly Transform _playerTransform;
 
+        public Vector3 PlayerPosition => _playerTransform.position;
+        public Vector3 TargetPosition { get; private set; }
+        public bool CanUpdate => _playerTransform != null;
+
+        /// <summary>案内対象のプレイヤーと目的地を保持する</summary>
+        /// <param name="playerTransform">案内対象のプレイヤーのTransform</param>
+        /// <param name="targetPosition">目的地の初期ワールド座標</param>
+        /// <example>LifetimeScopeからnew NavigationArrowService(playerTransform, targetPosition)で生成する</example>
+        public NavigationArrowService(Transform playerTransform, Vector3 targetPosition)
+        {
+            _playerTransform = playerTransform;
+            TargetPosition = targetPosition;
         }
 
-        /// <summary>
-        /// XZ平面上のfromからtoへの方向を計算し、角度を返す
-        /// </summary>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        /// <returns></returns>
-        private float CalculateArrowDirection(Vector3 from, Vector3 to)
+        /// <summary>案内先の目的地を変更する</summary>
+        /// <param name="targetPosition">新しい目的地のワールド座標</param>
+        /// <example>SetTargetPosition(new Vector3(10f, 0f, 5f))</example>
+        public void SetTargetPosition(Vector3 targetPosition)
         {
-            Vector3 direction = to - from;
-            direction.Normalize();
-            return Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+            TargetPosition = targetPosition;
+        }
+
+        /// <summary>高低差を除いた目的地方向を計算する</summary>
+        /// <param name="from">プレイヤーのワールド座標</param>
+        /// <returns>XZ平面上の単位ベクトル、水平位置が一致するときはゼロ</returns>
+        /// <example>CalculateArrowDirection(PlayerPosition)</example>
+        public Vector3 CalculateArrowDirection(Vector3 from)
+        {
+            // 保持している目的地への方向から高低差を取り除く
+            Vector3 direction = TargetPosition - from;
+            direction.y = 0f;
+            return direction.normalized;
         }
     }
 }
